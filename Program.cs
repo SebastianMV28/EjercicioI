1e47729276d0942daeb90e2d4792401955df0524
usuario <usuario@EDI104EST02>
2023-08-19 11:24:05 -0500

    n1 and n2

--- Program.cs
+++ Program.cs
@@ -4,11 +4,13 @@
     {
         static void Main(string[] args)
         {
-            ddd
+            
             Console.WriteLine("Hello, World!");
             for (int i = 0; i < 10; i++) {
                 Console.WriteLine("index is equal to" + i);
             }
+
+             int n1 = 2, n2 = 3;
         }
     }
 }
