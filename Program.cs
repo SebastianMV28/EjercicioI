4e38d0f7ec017b68590ed99c56fe1b96603ca68c
usuario <usuario@EDI104EST02>
2023-08-19 11:20:56 -0500

    commit y pull satisfactorio

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            
+            ddd
             Console.WriteLine("Hello, World!");
             for (int i = 0; i < 10; i++) {
                 Console.WriteLine("index is equal to" + i);
