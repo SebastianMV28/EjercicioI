aa7d423299743daf48d03b59972d5bc1ad639e38
Braian Acero <b.rayansteven2010@gmail.com>
2023-09-23 11:52:22 -0500

    trying to fix an error

--- Program.cs
+++ Program.cs
@@ -2,66 +2,80 @@
 {
     internal class Program
     {
-        static void Main(string[] args) { 
+        static void Main(string[] args)
+        {
 
 
             String userSelection = Console.ReadLine();
 
-        //Menu//
-        Console.WriteLine("Please, select an option ");
-            Console.WriteLine("0");
-            Console.WriteLine("1");
-            Console.WriteLine("2");
-            Console.WriteLine("3");
-            Console.WriteLine("4");
-            Console.WriteLine("5");
-            Console.WriteLine("6");
-            Console.WriteLine("7");
-            Console.WriteLine("8");
-            Console.WriteLine("9");
-            Console.WriteLine("10");
+            //Menu//
+            Console.WriteLine("Please, select an option ");
+           
+            Console.WriteLine("1- Add Number");
+            Console.WriteLine("2- ");
+            Console.WriteLine("3- ");
+            Console.WriteLine("4- Prom");
+            Console.WriteLine("5- ");
+            Console.WriteLine("6- ");
+            Console.WriteLine("7- ");
+            Console.WriteLine("8- ");
+            Console.WriteLine("9- ");
+            Console.WriteLine("10- ");
             Console.WriteLine("...........");
-            
+
             //Switch//
 
-            switch(userSelection){
+            switch (userSelection)
+            {
                 case "1":
                     Console.WriteLine("ingrese un numero");
 
                     int v = int.Parse(Console.ReadLine());
-                    int n= v;
+                    int n = v;
                     int opt1 = AddNumber(v);
-                   
+
                     Console.WriteLine(v);
                     break;
-                    case "2":
-                     
-        int opt2 = RandomPlusPorcent();
-                break;
-                    case "3":
-                        
-        int opt3 = FifthyPorcentRandom();
-                        break;
-                         case "4":
-                  int opt4 = Prom();
-        Console.writeLine(prom());
+                case "2":
+
+                    int opt2 = RandomPlusPorcent(); 
+                    break;
+                case "3":
+
+                    int opt3 = FifthyPorcentRandom();
+                    break;
+                case "4":
+                    int x = int.Parse(Console.ReadLine());
+                    int y = int.Parse(Console.ReadLine());
+                    int z = int.Parse(Console.ReadLine());
+                    int opt4 = Prom(x,y,z);
+                    Console.WriteLine(opt4);
                     ;
-                break;case "5":
-                    opt5= AddNumber();
-                break;case "6":
-                    opt6= AddNumber();
-                break;case "7":
-                    opt7= AddNumber();
-                break;case "8":
-                    opt8= AddNumber();
-                break;case "9":
-                    opt9= AddNumber();
-                break;case "10":
-                    opt10= AddNumber();
-                break;
+                    break;
+                case "5":
+                    int opt5 = AddNumber();
+                    break;
+                case "6":
+                    int opt6 = AddNumber();
+                    break;
+                case "7":
+                    int opt7 = AddNumber();
+                    break;
+                case "8":
+                    int opt8 = AddNumber();
+                    break;
+                case "9":
+                    int opt9 = AddNumber();
+                    break;
+                case "10":
+                    int opt10 = AddNumber();
+                    break;
                 default:
-                    Console.WriteLine("")
+                    Console.WriteLine("No valid option selected");
                     ;
+                    break;
+
+
 
             };
 
@@ -69,52 +83,50 @@
 
 
 
-    ///Methods///
+            ///Methods///
 
-    static int AddNumber(int x)
-    {
-        
-        
-
-        return x++;
-        
-    };
-
-    static void RandomPlusPorcent()
-    {
-        Random rnd = new Random();
-        double percent = 1.3;
-
-        int nRandom = rnd.Next(0, 200);
-
-        double porcentaje = nRandom * percent;
-
-        Console.WriteLine(nRandom.ToString());
-        Console.WriteLine(porcentaje.ToString());
-    }
-
-    static void FifthyPorcentRandom()
-    {
-        Random random = new Random();
-        int nRnd = random.Next(10, 50);
-
-        int fifteenPorcent = 15;
-        float finalPorcent = (nRnd * fifteenPorcent) / 100;
-        Console.WriteLine(nRnd);
-        Console.WriteLine(nRnd - finalPorcent);
-
-    }
+            static int AddNumber(int x)
+            {
 
 
-    static int Prom(int a, b, c)
-    {
-        a = int.Parse(Console.ReadLine());
-        b = int.Parse(Console.ReadLine());
-        c = int.Parse(Console.ReadLine());
 
-        int prom = (a + b + c) / 3;
-        return prom;
-    }
+                return x++;
+
+            };
+
+            static void RandomPlusPorcent()
+            {
+                Random rnd = new Random();
+                double percent = 1.3;
+
+                int nRandom = rnd.Next(0, 200);
+
+                double porcentaje = nRandom * percent;
+
+                Console.WriteLine(nRandom.ToString());
+                Console.WriteLine(porcentaje.ToString());
+            }
+
+            static void FifthyPorcentRandom()
+            {
+                Random random = new Random();
+                int nRnd = random.Next(10, 50);
+
+                int fifteenPorcent = 15;
+                float finalPorcent = (nRnd * fifteenPorcent) / 100;
+                Console.WriteLine(nRnd);
+                Console.WriteLine(nRnd - finalPorcent);
+
+            }
+
+
+            static int Prom(int a, int b, int c)
+            {
+               
+
+                int prom = (a + b + c) / 3;
+                return prom;
+            }
 
 
 
@@ -124,151 +136,152 @@
 
 
 
-    ///////////////////////////////////////////////////////
-    //Decimo Ejercicio
-    int pNumber, sNumber;
-    Console.WriteLine("Ingrese el primer numero");
-            pNumber= int.Parse(Console.ReadLine());
-    Console.WriteLine("Ingrese el segundo numero");
-            sNumber= int.Parse(Console.ReadLine());
-             if (sNumber % pNumber== 0) {
+            ///////////////////////////////////////////////////////
+            //Decimo Ejercicio
+            int pNumber, sNumber;
+            Console.WriteLine("Ingrese el primer numero");
+            pNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero");
+            sNumber = int.Parse(Console.ReadLine());
+            if (sNumber % pNumber == 0)
+            {
                 Console.WriteLine("Es divisible");
             }
             else
-{
-    Console.WriteLine("No es divisible");
-}
+            {
+                Console.WriteLine("No es divisible");
+            }
 
 
 
-//Noveno Ejercicio
-double hombres, mujeres;
+            //Noveno Ejercicio
+            double hombres, mujeres;
 
-Console.WriteLine("Ingrese la cantidad de hombres en el grupo");
-hombres = double.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese la cantidad de mujeres en el grupo");
-mujeres = double.Parse(Console.ReadLine());
-double grupoTotal = hombres + mujeres;
-
-
-hombres = (hombres / grupoTotal) * 100;
-mujeres = (mujeres / grupoTotal) * 100;
-
-Console.WriteLine("El porcentaje de hombres es :" + hombres + "%" + " y el porcentaje de mujeres es :" + mujeres + "%");
+            Console.WriteLine("Ingrese la cantidad de hombres en el grupo");
+            hombres = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de mujeres en el grupo");
+            mujeres = double.Parse(Console.ReadLine());
+            double grupoTotal = hombres + mujeres;
 
 
+            hombres = (hombres / grupoTotal) * 100;
+            mujeres = (mujeres / grupoTotal) * 100;
 
-//Octavo Ejercicio
-double fahrenheit, celsius;
-Console.WriteLine("Ingrese la temperatura en grados Celsius");
-celsius = double.Parse(Console.ReadLine());
-fahrenheit = (celsius * 1.8) + 32;
-Console.WriteLine("La temperatura convertida a Fahrenheit es de: " + fahrenheit);
+            Console.WriteLine("El porcentaje de hombres es :" + hombres + "%" + " y el porcentaje de mujeres es :" + mujeres + "%");
 
 
 
-//Septimo Ejercicio
-double cms, yardas, metros, pies, pulgadas;
-
-Console.WriteLine("Ingrese los centimetros que desea convertir");
-cms = double.Parse(Console.ReadLine());
-metros = cms * 0.01;
-yardas = cms * 0.0109361;
-pies = cms * 0.0328084;
-pulgadas = cms * 0.393701;
-Console.WriteLine("El valor de los centimetros en pulgadas es " + (Math.Pow(pulgadas, 1)));
-Console.WriteLine("El valor de los centimetros en metros es " + (Math.Pow(metros, 1)));
-Console.WriteLine("El valor de los centimetros en yardas es " + (Math.Pow(yardas, 1)));
-Console.WriteLine("El valor de los centimetros en pies es " + (Math.Pow(pies, 1)));
-
-//Sexto Ejercicio
-
-int radio, altura;
-double volumen;
-
-Console.WriteLine("Ingrese el radio del cilindro ");
-radio = int.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese la altura del cilindro");
-altura = int.Parse(Console.ReadLine());
-
-do
-{
-    Console.WriteLine("Ingrese un valor para radio mayor a 0");
-    radio = int.Parse(Console.ReadLine());
-    Console.WriteLine("Ingrese un valor para altura mayor a 0");
-    altura = int.Parse(Console.ReadLine());
-} while (radio < 0 || altura < 0);
-
-
-double areaB = Math.PI * (Math.Pow(radio, 2));
-
-volumen = Math.Round(areaB * altura, 2);
-Console.WriteLine("El Volumen del area es " + volumen);
+            //Octavo Ejercicio
+            double fahrenheit, celsius;
+            Console.WriteLine("Ingrese la temperatura en grados Celsius");
+            celsius = double.Parse(Console.ReadLine());
+            fahrenheit = (celsius * 1.8) + 32;
+            Console.WriteLine("La temperatura convertida a Fahrenheit es de: " + fahrenheit);
 
 
 
-//Quinto Ejercicio
+            //Septimo Ejercicio
+            double cms, yardas, metros, pies, pulgadas;
 
-int bases;
-int height;
+            Console.WriteLine("Ingrese los centimetros que desea convertir");
+            cms = double.Parse(Console.ReadLine());
+            metros = cms * 0.01;
+            yardas = cms * 0.0109361;
+            pies = cms * 0.0328084;
+            pulgadas = cms * 0.393701;
+            Console.WriteLine("El valor de los centimetros en pulgadas es " + (Math.Pow(pulgadas, 1)));
+            Console.WriteLine("El valor de los centimetros en metros es " + (Math.Pow(metros, 1)));
+            Console.WriteLine("El valor de los centimetros en yardas es " + (Math.Pow(yardas, 1)));
+            Console.WriteLine("El valor de los centimetros en pies es " + (Math.Pow(pies, 1)));
 
-Console.WriteLine("Ingrese la base del rectangulo");
-bases = int.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese la altura del rectangulo");
-height = int.Parse(Console.ReadLine());
+            //Sexto Ejercicio
 
-int perimeter = 2 * bases + 2 * height;
-int area = bases * height;
-Console.WriteLine("El perimetro del rectangulo es " + perimeter);
-Console.WriteLine("El area del rectangulo es " + area);
+            int radio, altura;
+            double volumen;
 
+            Console.WriteLine("Ingrese el radio del cilindro ");
+            radio = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la altura del cilindro");
+            altura = int.Parse(Console.ReadLine());
 
-
-//Cuarto Ejercicio
-int primer, segundo, tercero;
-
-primer = int.Parse(Console.ReadLine());
-segundo = int.Parse(Console.ReadLine());
-tercero = int.Parse(Console.ReadLine());
-
-int prom = (primer + segundo + tercero) / 3;
-
-Console.WriteLine(prom);
-
-
-
-//Tercer Ejercicio
-
-Random random = new Random();
-int nRnd = random.Next(10, 50);
-
-int fifteenPorcent = 15;
-float finalPorcent = (nRnd * fifteenPorcent) / 100;
-Console.WriteLine(nRnd);
-Console.WriteLine(nRnd - finalPorcent);
+            do
+            {
+                Console.WriteLine("Ingrese un valor para radio mayor a 0");
+                radio = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese un valor para altura mayor a 0");
+                altura = int.Parse(Console.ReadLine());
+            } while (radio < 0 || altura < 0);
 
 
-//Segundo Ejercicio
+            double areaB = Math.PI * (Math.Pow(radio, 2));
 
-Random rnd = new Random();
-double percent = 1.3;
+            volumen = Math.Round(areaB * altura, 2);
+            Console.WriteLine("El Volumen del area es " + volumen);
 
-int nRandom = rnd.Next(0, 200);
 
-double porcentaje = nRandom * percent;
 
-Console.WriteLine(nRandom.ToString());
-Console.WriteLine(porcentaje.ToString());
+            //Quinto Ejercicio
 
-//Primer Ejercicio
-int n1;
-Console.WriteLine("ingrese un numero");
-n1 = int.Parse(Console.ReadLine());
+            int bases;
+            int height;
 
-n1++;
-Console.WriteLine(n1);
+            Console.WriteLine("Ingrese la base del rectangulo");
+            bases = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la altura del rectangulo");
+            height = int.Parse(Console.ReadLine());
 
-            
+            int perimeter = 2 * bases + 2 * height;
+            int area = bases * height;
+            Console.WriteLine("El perimetro del rectangulo es " + perimeter);
+            Console.WriteLine("El area del rectangulo es " + area);
+
+
+
+            //Cuarto Ejercicio
+            int primer, segundo, tercero;
+
+            primer = int.Parse(Console.ReadLine());
+            segundo = int.Parse(Console.ReadLine());
+            tercero = int.Parse(Console.ReadLine());
+
+            int prom = (primer + segundo + tercero) / 3;
+
+            Console.WriteLine(prom);
+
+
+
+            //Tercer Ejercicio
+
+            Random random = new Random();
+            int nRnd = random.Next(10, 50);
+
+            int fifteenPorcent = 15;
+            float finalPorcent = (nRnd * fifteenPorcent) / 100;
+            Console.WriteLine(nRnd);
+            Console.WriteLine(nRnd - finalPorcent);
+
+
+            //Segundo Ejercicio
+
+            Random rnd = new Random();
+            double percent = 1.3;
+
+            int nRandom = rnd.Next(0, 200);
+
+            double porcentaje = nRandom * percent;
+
+            Console.WriteLine(nRandom.ToString());
+            Console.WriteLine(porcentaje.ToString());
+
+            //Primer Ejercicio
+            int n1;
+            Console.WriteLine("ingrese un numero");
+            n1 = int.Parse(Console.ReadLine());
+
+            n1++;
+            Console.WriteLine(n1);
+
+
 
 
 
